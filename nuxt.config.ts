a38469cb1236162831c33605db133ca675efc2c4
Richard Huynh <emailofrichardh@gmail.com>
2024-10-26 10:43:56 -0400

    First commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+
+  modules: ['@pinia/nuxt'],
+  compatibilityDate: '2024-10-26',
 });
