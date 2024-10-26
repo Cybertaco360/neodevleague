e03f09d960a7df1685933c1b460c10008aa86edf
Richard Huynh <emailofrichardh@gmail.com>
2024-10-26 11:39:36 -0400

    Whatever

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-const route = useRoute()
+  const route = useRoute()
 </script>
 
-<script src="scripts.js"></script>
+<script src="../components/scripts.js"></script>
 
 <template>
   <div>
-    <button @click="voiceInput">Microphone Image Placeholder</button>
+    <button>Microphone Image Placeholder</button>
     <span>Text Box Placeholder</span>
   </div>
 </template>
